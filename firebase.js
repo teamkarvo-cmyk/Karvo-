5da9165ae3f1c6654cd5e487987f89f5b0420fda
teamkarvo-cmyk <teamkarvo@gmail.com>
2026-07-06 20:14:17 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCbKuhaoqCMrKWbzsGT9Vb3EjFdFjjbyEw",
+  authDomain: "karvo-86279.firebaseapp.com",
+  projectId: "karvo-86279",
+  storageBucket: "karvo-86279.firebasestorage.app",
+  messagingSenderId: "105369503187",
+  appId: "1:105369503187:web:c97fb2922b54fff1959b26"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
