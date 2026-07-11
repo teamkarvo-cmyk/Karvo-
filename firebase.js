32796aa9774e52d57c6714b2b5cd9c055ed27921
teamkarvo-cmyk <teamkarvo@gmail.com>
2026-07-11 19:57:59 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -69,11 +69,7 @@ async function loadWorkers() {
   try {
   const querySnapshot = await getDocs(collection(db, "workers"));
     workerGrid.innerHTML = "";
-alert("Documents: " + querySnapshot.size);
-console.log(querySnapshot.size);
-    querySnapshot.forEach((doc) => {
-      const worker = doc.data();
-      alert(JSON.stringify(worker));
+
       console.log(worker);
     
 
