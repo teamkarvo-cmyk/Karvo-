84889ca7ceff220bd41854efe8fd2768a8b668e4
teamkarvo-cmyk <teamkarvo@gmail.com>
2026-07-14 10:52:55 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,7 +8,8 @@ import {
 import {
   getFirestore,
   collection,
-  getDocs
+  getDocs,
+  addDoc
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 const firebaseConfig = {
   apiKey: "AIzaSyCbKuhaoqCMrKWbzsGT9Vb3EjFdFjjbyEw",
@@ -23,87 +24,36 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 // SIGNUP
-const signupBtn = document.getElementById("signupBtn");
+  const signupBtn = document.getElementById("signupBtn");
 
 if (signupBtn) {
-  signupBtn.addEventListener("click", () => {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+  signupBtn.addEventListener("click", async () => {
+    try {
+      const name = document.getElementById("name").value;
+      const phone = document.getElementById("phone").value;
+      const city = document.getElementById("city").value;
+      const category = document.getElementById("category").value;
+      const experience = Number(document.getElementById("experience").value);
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
 
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(() => {
-        alert("Account Created Successfully");
-        window.location.href = "login.html";
-      })
-      .catch((error) => {
-        alert(error.message);
+      await createUserWithEmailAndPassword(auth, email, password);
+
+      await addDoc(collection(db, "Workers"), {
+        Name: name,
+        Phone: phone,
+        City: city,
+        category: category,
+        Experience: experience,
+        rating: 4.9,
+        Verified: true
       });
+
+      alert("Account Created Successfully");
+      window.location.href = "login.html";
+
+    } catch (error) {
+      alert(error.message);
+    }
   });
-}
-
-// LOGIN
-const loginBtn = document.getElementById("loginBtn");
-
-if (loginBtn) {
-  loginBtn.addEventListener("click", () => {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    signInWithEmailAndPassword(auth, email, password)
-      .then(() => {
-        alert("Login Successful");
-        window.location.href = "index.html";
-      })
-      .catch((error) => {
-        alert(error.message);
-      });
-  });
-}
-const workerGrid = document.getElementById("workerGrid");
-
-
-if (workerGrid) {
-  
-  loadWorkers();
   }
-
-
-async function loadWorkers() {
-  try {
-    const querySnapshot = await getDocs(collection(db, "Workers"));
-
-    
-
-    workerGrid.innerHTML = "";
-
-    querySnapshot.forEach((doc) => {
-
-      
-
-      const worker = doc.data();
-      console.log(worker.Phone);
-
-      const card = `
-<div class="card">
-  <h3>👷 ${worker.Name}</h3>
-  <p>🛠 ${worker.category}</p>
-  <p>⭐ ${worker.rating}</p>
-  <p>📍 ${worker.City}</p>
-  <p>✔ Verified</p>
-
-  <div class="card-buttons">
-    <a href="Worker.html?phone=${worker.Phone}" class="view-btn">View Profile</a>
-    <button onclick="window.location.href='tel:${worker.Phone}'">Call</button>
-  </div>
-</div>
-`;
-
-      workerGrid.innerHTML += card;
-    });
-
-  } catch (error) {
-    alert(error.message);
-    console.log(error);
-    
-  }
-}
